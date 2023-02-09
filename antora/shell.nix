4c7c6d62812f58d7f8e8724d6cf3e502363e5e2c
Leo Soares <leo@hyper.systems>
2023-02-09 14:52:50 +0000

    Pin nixpkgs to current `nixos-22.11-small`

--- antora/shell.nix
+++ antora/shell.nix
@@ -1,3 +1,6 @@
-{ pkgs ? import <nixpkgs> { } }:
+# Commit hash for nixos-22.11-small obtained at https://status.nixos.org as of 12-01-2023
+{ pkgs ? import
+  (fetchTarball "https://github.com/NixOS/nixpkgs/archive/7010ce2ebc72.tar.gz")
+  { } }:
 
 pkgs.mkShell { buildInputs = [ pkgs.nodejs-18_x ]; }
